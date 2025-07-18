2460c4f65b768c0b52ce3811294e5d9fe2a467ad
Joseph <JPMeehan5@Gmail.com>
2025-07-18 14:50:04 -0700

    Roll Flavor

--- module/rolls/attribute-roll.mjs
+++ module/rolls/attribute-roll.mjs
@@ -1,3 +1,15 @@
 import MythCraftRoll from "./base-roll.mjs";
 
-export default class AttributeRoll extends MythCraftRoll {}
+export default class AttributeRoll extends MythCraftRoll {
+  constructor(formula = "1d20", data = {}, options = {}) {
+    console.log(options);
+    if (!options.flavor && options.attribute) {
+      const abbr = game.i18n.localize(`MYTHCRAFT.Actor.base.FIELDS.attributes.${options.attribute}.label`);
+
+      options.flavor = options.skill ?
+        game.i18n.format("MYTHCRAFT.Roll.AttributeRoll.Flavor.Skill", { abbr, skill: game.i18n.localize(mythcraft.CONFIG.skills.list[options.skill]?.label) }) :
+        game.i18n.format("MYTHCRAFT.Roll.AttributeRoll.Flavor.Attribute", { abbr });
+    }
+    super(formula, data, options);
+  }
+}
