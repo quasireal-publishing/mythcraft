917f9416ab9295ab2a2439b9e366a4788e504352
Joseph <JPMeehan5@Gmail.com>
2025-10-29 11:35:17 -0700

    Removed transformName handler

--- tools/pushLDBtoJSON.mjs
+++ tools/pushLDBtoJSON.mjs
@@ -4,7 +4,7 @@ import path from "path";
 
 const SYSTEM_ID = process.cwd();
 const yaml = false;
-const expandAdventures = false;
+const expandAdventures = true;
 const folders = true;
 
 const packs = await fs.readdir("./packs");
@@ -16,7 +16,6 @@ for (const pack of packs) {
     `${SYSTEM_ID}/src/packs/${pack}`,
     {
       yaml,
-      transformName,
       transformEntry,
       expandAdventures,
       folders,
@@ -24,21 +23,6 @@ for (const pack of packs) {
       omitVolatile: true,
     },
   );
-}
-
-/**
- * Prefaces the document with its type.
- * @param {object} doc - The document data.
- */
-function transformName(doc, context) {
-  const safeFileName = doc.name.replace(/[^a-zA-Z0-9А-я]/g, "_");
-  // If we add adventures and enable "expand adventures" it will need handling
-  const type = doc._key?.split("!")[1];
-
-  const prefix = ["actors", "items"].includes(type) ? doc.type : type;
-  let name = `${doc.name ? `${prefix}_${safeFileName}_${doc._id}` : doc._id}.${yaml ? "yml" : "json"}`;
-  if (context.folder) name = path.join(context.folder, name);
-  return name;
 }
 
 /**
